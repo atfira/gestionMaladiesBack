bb30a4e1871f1541efd9d2b4c4a6fa6448ef88cb
atfira <oussama.bahlouli@outlook.com>
2019-12-09 22:47:44 +0100

    first commit

---
+++ src/main/java/com/oussama/GestionMaladiesBackApplication.java
@@ -0,0 +1,13 @@
+package com.oussama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionMaladiesBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionMaladiesBackApplication.class, args);
+	}
+
+}
